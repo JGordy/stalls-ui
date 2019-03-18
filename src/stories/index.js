32cbe62cc2be7c06392cd4c4bb168c8642bb829e
Joseph Gordy <jgordy2424@gmail.com>
2019-03-17 23:10:38 -0400

    Organizational changes

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Button from "../../src/features/common/Button";
+// import React from "react";
+// import { storiesOf } from "@storybook/react";
+// import Button from "../../src/features/common/Button";
 
-storiesOf("Button", module)
-    .add("active with fill", () => (
-        <Button label={`continue`} fill={true} active={true} />
-    ))
-    .add("active with no fill", () => (
-        <Button label={`sign up`} fill={false} active={true} />
-    ))
-    .add("disabled", () => <Button label={`continue`} active={false} />);
+// storiesOf("Button", module)
+//     .add("active with fill", () => (
+//         <Button label={`continue`} fill={true} active={true} />
+//     ))
+//     .add("active with no fill", () => (
+//         <Button label={`sign up`} fill={false} active={true} />
+//     ))
+//     .add("disabled", () => <Button label={`continue`} active={false} />);
