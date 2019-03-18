5ea441d99e6694b2de6472d2d15fe9fd9eec6017
Joseph Gordy <jgordy2424@gmail.com>
2019-03-17 23:10:54 -0400

    Add new components to storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
 import { configure } from "@storybook/react";
 
 function loadStories() {
-    require("../src/stories/index.js");
+    require('../src/stories/index.js');
+    require('../src/stories/Button');
+    require('../src/stories/MiniButton');
+    require('../src/stories/ProfileImage');
     // You can require as many stories as you need.
 }
 
