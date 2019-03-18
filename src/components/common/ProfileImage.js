9f515028c84a9a476c3ce70462043a13284e64bd
Joseph Gordy <jgordy2424@gmail.com>
2019-03-17 23:10:10 -0400

    Create ProfileImage component

---
+++ src/components/common/ProfileImage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import FontAwesome from '../FontAwesome/FontAwesome';
+import '../../styles/ProfileImage.css';
+
+const ProfileImage = ({
+    roundImage,
+    mainImage,
+    BGimage
+}) => (
+        <div
+            className="ProfileImage"
+            style={{
+                borderRadius: roundImage ? '50%' : '10%',
+                backgroundImage: mainImage && `url(${mainImage})`,
+                backgroundColor: BGimage ? '' : 'var(--white)',
+                display: mainImage ? 'block' : 'flex',
+                alignContent: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            {/* !mainImage && <FontAwesome icon="camera" /> */}
+        </div>
+    );
+
+ProfileImage.propTypes = {};
+
+ProfileImage.defaultProps = {};
+
+export default ProfileImage;
---
+++ src/stories/ProfileImage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import ProfileImage from "../../src/components/common/ProfileImage";
+
+storiesOf("ProfileImage", module)
+    .add("default", () => (
+        <ProfileImage mainImage={'https://homepages.cae.wisc.edu/~ece533/images/girl.png'} />
+    ))
+    .add("rounded", () => (
+        <ProfileImage roundImage={true} mainImage={'https://homepages.cae.wisc.edu/~ece533/images/girl.png'} />
+    ))
+    .add("missing url fallback", () => (
+        <ProfileImage mainImage={''} />
+    ))
