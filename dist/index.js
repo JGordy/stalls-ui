72897634ee73a6507c17c4b5a6e2958901853f04
Joseph Gordy <jgordy2424@gmail.com>
2019-09-06 14:42:40 -0400

    Update package.json publish command

--- dist/index.js
+++ dist/index.js
@@ -1,79 +1,21 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+var _react = require('react');
 
-var _Button = require('./common/src/Button');
+var _react2 = _interopRequireDefault(_react);
 
-Object.defineProperty(exports, 'Button', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Button).default;
-  }
-});
+var _reactDom = require('react-dom');
 
-var _Glyphicon = require('./common/src/Glyphicon');
+var _reactDom2 = _interopRequireDefault(_reactDom);
 
-Object.defineProperty(exports, 'Glyphicon', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Glyphicon).default;
-  }
-});
+var _App = require('./App');
 
-var _MiniButton = require('./common/src/MiniButton');
+var _App2 = _interopRequireDefault(_App);
 
-Object.defineProperty(exports, 'MiniButton', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_MiniButton).default;
-  }
-});
+require('./styles/index.css');
 
-var _Modal = require('./common/src/Modal');
-
-Object.defineProperty(exports, 'Modal', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_Modal).default;
-  }
-});
-
-var _ModalContent = require('./common/src/ModalContent');
-
-Object.defineProperty(exports, 'ModalContent', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_ModalContent).default;
-  }
-});
-
-var _ModalTrigger = require('./common/src/ModalTrigger');
-
-Object.defineProperty(exports, 'ModalTrigger', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_ModalTrigger).default;
-  }
-});
-
-var _ProfileImage = require('./common/src/ProfileImage');
-
-Object.defineProperty(exports, 'ProfileImage', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_ProfileImage).default;
-  }
-});
-
-var _TextInput = require('./common/src/TextInput');
-
-Object.defineProperty(exports, 'TextInput', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_TextInput).default;
-  }
-});
+require('config/fontawesome');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_reactDom2.default.render(_react2.default.createElement(_App2.default, null), document.getElementById("root"));
