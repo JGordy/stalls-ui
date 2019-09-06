695bea6b2338c5176b1b59091ad59fffdc3a50ad
Joseph Gordy <jgordy2424@gmail.com>
2019-09-06 14:58:12 -0400

    export components from index replacing previous logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles/index.css';
-
-import 'config/fontawesome';
-
-ReactDOM.render(<App />, document.getElementById("root"));
+export { default as Button } from './components/common/Button';
+export { default as Glyphicon } from './components/common/Glyphicon';
+export { default as MiniButton } from './components/common/MiniButton';
+export { default as Modal } from './components/common/Modal';
+export { default as ModalContent } from './components/common/ModalContent';
+export { default as ModalTrigger } from './components/common/ModalTrigger';
+export { default as ProfileImage } from './components/common/ProfileImage';
+export { default as TextInput } from './components/common/TextInput';
