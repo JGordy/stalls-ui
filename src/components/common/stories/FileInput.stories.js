247422cdfb0f5c8135fedce30845168cb6f31fc3
Joseph Gordy <jgordy2424@gmail.com>
2019-09-20 19:32:10 -0400

    Update to storybook 5.2. Add docgen configs

--- src/components/common/stories/FileInput.stories.js
+++ src/components/common/stories/FileInput.stories.js
@@ -2,28 +2,31 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { FileInput } from '../src';
 
-// import results from '../../../../src/jest-test-results.json';
-// import { withTests } from '@storybook/addon-jest';
-
-const buttonProps = {
-    bsStyle: 'success',
-    icon: 'upload',
-};
-
-const inputProps = {
-    classes: 'active another_class',
-    inputName: 'profile',
-    onChange: (event) => console.info("File", event.target.files),
-    ...buttonProps
-};
+import results from '../../../../src/jest-test-results.json';
+import { withTests } from '@storybook/addon-jest';
 
 storiesOf('FileInput', module)
-    // .addDecorator(withTests({ results }))
-    // .addParameters({ jest: ['FileInput.spec.js'] })
+    .addDecorator(withTests({ results }))
+    .addParameters({ jest: ['FileInput.spec.js'] })
     .add('Default', () => {
+
+        const buttonProps = {
+            bsStyle: 'success',
+            icon: 'upload',
+        };
+
+        const inputProps = {
+            classes: 'active another_class',
+            inputName: 'profile',
+            onChange: (event) => console.info("File", event.target.files),
+        };
+
         return (
             <React.Fragment>
-                <FileInput {...inputProps} />
+                <FileInput
+                    {...inputProps}
+                    {...buttonProps}
+                />
                 <p>Check out the dev console once you have selected a file!</p>
             </React.Fragment>
         );
