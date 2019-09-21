72a1eb350ee2ee783881f97085b9716eb138409f
Joseph Gordy <jgordy2424@gmail.com>
2019-09-20 22:26:02 -0400

    Show example of children text in HeroImage component

--- src/components/common/stories/HeroImage.stories.js
+++ src/components/common/stories/HeroImage.stories.js
@@ -9,5 +9,8 @@ storiesOf("HeroImage", module)
         <HeroImage
             imgSrc='https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000'
             altText='Yosemite National Park'
-        />
+            overlayColor='rgba(0,0,0,0.5)'
+        >
+            Here's some text
+        </HeroImage>
     ));
