c643def25b0a6e535a63e2fa685b487400c47f04
Joseph Gordy <jgordy2424@gmail.com>
2022-06-01 10:59:28 -0400

    Update and Add required packages

--- src/testUtils/standard-tests.js
+++ src/testUtils/standard-tests.js
@@ -16,6 +16,49 @@ const canReceiveClass = (testElement, options) => {
     });
 }
 
+export const checkConsoleWarnOrErrors = (hasAllowableConsole = false) => {
+    // RUn this seperately for each block/describe/test if there are allowable errors
+    let isConsoleWarningOrError;
+    let hasReactWarningOrError;
+
+    beforeEach(() => {
+        isConsoleWarningOrError = false;
+        hasReactWarningOrError = false;
+
+        jest.spyOn(global.console, 'error').mockImplementation((...args) => {
+            isConsoleWarningOrError = true;
+            args.forEach((err) => {
+                if (typeof err === 'string' && err.toLowerCase().includes('react')) {
+                    hasReactWarningOrError = true;
+                }
+            })
+            // Optional: jest spits out the error anyways
+            console.log(...args);
+        });
+
+        jest.spyOn(global.console, 'warn').mockImplementation((...args) => {
+            isConsoleWarningOrError = true;
+            args.forEach((err) => {
+                if (typeof err === 'string' && err.toLowerCase().includes('react')) {
+                    hasReactWarningOrError = true;
+                }
+            })
+            // Optional: jest spits out the error anyways
+            console.log(...args);
+        })
+    });
+
+    afterEach(() => {
+        if (isConsoleWarningOrError && !hasAllowableConsole) {
+            let errorMessage = 'Console warnings and errors are not allowed';
+            if (hasReactWarningOrError) {
+                errorMessage = 'React console warnings or errors are not allowed';
+            }
+            throw new Error(errorMessage);
+        }
+    });
+}
+
 export const runStandardComponentTests = (testElement, options) => {
     describe('Standard component tests', () => {
         componentExists(testElement, options);
